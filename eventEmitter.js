a8c9d6ed935e07930a2b1ade59c29dcda3253b10
zhiguangphoenix <czg18366118377@yahoo.com>
2018-10-25 18:25:37 +0800

    实现解绑函数off、offAll；一次性绑定函数once等等

--- eventEmitter.js
+++ eventEmitter.js
@@ -12,18 +12,16 @@ class eventEmitter {
   }
 
   once(eventName, listener) {
+    // 使用一个新函数包装，先移除该listener，再运行该listener，保证它只运行一次
+    let fn = () => {
+      this.off(eventName, listener);
+      listener.apply(this, arguments);
+    }
+    fn.rawListener = listener;
 
+    this.on(eventName, fn);
+    return this;
   }
-
-  off(eventName, listener) {
-
-  }
-
-  offAll(event) {
-
-  }
-
-  
 
   emit(eventName, ...args) {
     let listeners;
@@ -32,19 +30,49 @@ class eventEmitter {
     }
 
     listeners = listeners.slice(0);
-    listeners.forEach(listener => {
-      listener.apply(this, args);
+    listeners.forEach(fn => {
+      fn.apply(this, args);
     });
 
     return this;
   }
+
+  off(eventName, listener) {
+    let listeners;
+    if (!this._events || !(listeners = this._events[eventName])) {
+      return this;
+    }
+
+    listeners.forEach((fn, index) => {
+      if (fn === listener || fn.rawListener === listener) {
+        listeners.splice(index, 1);
+      }
+    })
+
+    if (listeners.length === 0) {
+      delete this._events[eventName];
+    }
+
+    return this;
+  }
+
+  offAll(eventName) {
+    if (!this._events) {
+      return this;
+    } else if (typeof this._events[eventName] === 'undefined') {
+      let listenersArr = Object.values(this._events);
+      listenersArr.forEach(arr => {
+        arr.length = 0;
+      })
+    } else {
+      this._events[eventName].length = 0;
+    }
+  }
+
+  listeners(eventName) {
+    if (!eventName) return [];
+    return this._events[eventName];
+  }
 }
 
-let emitter = new eventEmitter();
-emitter.on('hit', function () {
-  console.log('hit111!!!');
-});
-emitter.on('hit', function () {
-  console.log('hit222!!!');
-});
-emitter.emit('hit');
+module.exports = eventEmitter;
